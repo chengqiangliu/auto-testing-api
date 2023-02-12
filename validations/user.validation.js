06a697284c8d7b05e55121289a73fbe9c390faef
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-02-12 17:55:25 +0900

    Update user.validation.js

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,84 +1,130 @@
-const { body } = require("express-validator");
+//removed body from the source code and added check and validation instead
+const { check, validationResult } = require("express-validator");
 
-const userLoginValidator = () => {
-    return [
-        body("username")
+exports.userLoginValidator =[
+        check("username")
             .exists({checkFalsy: true})
             .withMessage("User name is required")
             .isString()
             .withMessage("User name should be string"),
-        body("password")
+        check("password")
             .exists()
             .withMessage("Password is required")
-            .isString()
-            .withMessage("Password should be string")
+            //.isString()
+            .matches(
+                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/,
+              )
+            .withMessage("Password should be have atleast one upper case letter, one lower case letter, one number and one special character")
             .isLength({min: 5})
             .withMessage("Password should be at least 5 characters"),
+            
+        (req, res, next) => {
+                const error = validationResult(req).formatWith(({ msg }) => [{msg,errorcode:'401'}]);
+            
+                const hasError = !error.isEmpty();
+            
+                if (hasError) {
+                  res.status(422).json({ errors: error.array() });
+                } else {
+                  next();
+                }
+              }
     ]
-};
+;
 
-const userAddValidator = () => {
-    return [
-        body("username")
+exports.userAddValidator = 
+    [
+        check("username")
             .exists({checkFalsy: true})
             .withMessage("User name is required")
             .isString()
             .withMessage("User name should be string"),
-        body("password")
+        check("password")
             .exists()
             .withMessage("Password is required")
-            .isString()
-            .withMessage("Password should be string")
+            //.isString()
+            .matches(
+                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/,
+              )
+            .withMessage("Password should be have atleast one upper case letter, one lower case letter, one number and one special character")
             .isLength({min: 5})
             .withMessage("Password should be at least 5 characters"),
-        body("email").optional().isEmail().withMessage("Provide valid email"),
-        body("phone")
+        check("clientId")
+        .exists()
+        .withMessage("clientID is required"),
+        check("email").optional().isEmail().withMessage("Provide valid email"),
+        check("phone")
             .optional()
             .isString()
             .withMessage("phone number should be string"),
-    ]
-};
+            (req, res, next) => {
+                const error = validationResult(req).formatWith(({ msg }) => [{msg,errorcode:'401'}]);
 
-const userUpdateValidator = () => {
-    return [
-        body("_id")
+                const hasError = !error.isEmpty();
+
+                if (hasError) {
+                res.status(422).json({ errors: error.array() });
+                } else {
+                next();
+                }
+            }
+    ];
+
+exports.userUpdateValidator = 
+     [
+        check("_id")
             .exists({checkFalsy: true})
             .withMessage("User ID is required")
             .isString()
             .withMessage("User ID should be string"),
-        body("username")
+        check("username")
             .exists({checkFalsy: true})
             .withMessage("User name is required")
             .isString()
             .withMessage("User name should be string"),
-        body("password")
+        check("password")
             .exists()
             .withMessage("Password is required")
             .isString()
             .withMessage("Password should be string")
             .isLength({min: 5})
             .withMessage("Password should be at least 5 characters"),
-        body("email").optional().isEmail().withMessage("Provide valid email"),
-        body("phone")
+        check("email").optional().isEmail().withMessage("Provide valid email"),
+        check("phone")
             .optional()
             .isString()
             .withMessage("phone number should be string"),
-    ]
-};
+            (req, res, next) => {
+                const error = validationResult(req).formatWith(({ msg }) => [{msg,errorcode:'401'}]);
 
-const userDeleteValidator = () => {
-    return [
-        body("userId")
+                const hasError = !error.isEmpty();
+
+                if (hasError) {
+                res.status(422).json({ errors: error.array() });
+                } else {
+                next();
+                }
+            }
+    ];
+
+
+exports.userDeleteValidator = 
+     [
+        check("userId")
             .exists({checkFalsy: true})
             .withMessage("User ID is required")
             .isString()
             .withMessage("User ID should be string"),
-    ]
-};
+        (req, res, next) => {
+            const error = validationResult(req).formatWith(({ msg }) => [{msg,errorcode:'401'}]);
 
-module.exports = {
-    userLoginValidator,
-    userAddValidator,
-    userUpdateValidator,
-    userDeleteValidator
-};
+            const hasError = !error.isEmpty();
+
+            if (hasError) {
+            res.status(422).json({ errors: error.array() });
+            } else {
+            next();
+            }
+        }
+    ];
+
