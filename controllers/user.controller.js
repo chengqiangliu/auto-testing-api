d6db5398a73e7d07cdf9f7f1d11c270b23b5ccdf
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-02-12 17:51:07 +0900

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,61 +11,67 @@ const { validate } = require("./common.controller");
 const logger = require('../lib/logger').API;
 const { generateToken } = require('../auth');
 
-// 登陆
+//user functionalities
+//user login
 const userLogin = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
     logger.info('The user login controller is started');
     try {
         // validation
-        // const validateResult = validate(req, true);
-        // if (!validateResult.success) {
-        //     return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
-        // }
+        const validateResult = validate(req, true);
+        if (!validateResult.success) {
+            return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
+        }
 
-        // let {username, password} = req.body;
+        let {username, password} = req.body;
+       
+        //if username exists fetch the user information, if it doesn't exist and given username is admin, create a user named admin or else return an error
+        let user = await UserModel.findOne({username, password: md5(password)});
+        if (!user && username === 'admin') {
+            password = 'Admin1@'
+            await UserModel.create({username: 'admin', password: md5(password), clientId: '9999'});
+            logger.info('初始化用户: 用户名: admin 密码为: admin');
+            user = await UserModel.findOne({username, password: md5(password)});
+        }
 
-        // // 根据username和password查询数据库users, 如果没有, 返回提示错误的信息, 如果有, 返回登陆成功信息(包含user)
-        // let user = await UserModel.findOne({username, password: md5(password)});
-        // if (!user && username === 'admin') {
-        //     password = 'admin'
-        //     await UserModel.create({username: 'admin', password: md5(password), clientId: '9999'});
-        //     logger.info('初始化用户: 用户名: admin 密码为: admin');
-        //     user = await UserModel.findOne({username, password: md5(password)});
-        // }
-
-        // if(user) {
-        //     const client = await ClientModel.findOne({clientId: user.clientId});
-        //     if(client) {
-        //         const accessToken = generateToken({userId: user._id, createTime: new Date()}, 'AccessToken');
-        //         const refreshToken = generateToken({userId: user._id, createTime: new Date()}, 'RefreshToken');
-        //         await RefreshTokenModel.create({refreshToken, clientId: user.clientId, userId: user._id});
-        //         if (user.role_id) {
-        //             const role = await RoleModel.findOne({_id: user.role_id});
-        //             logger.info(`login success, ${user.username}`);
-        //             return res.status(200).json({success: true, data: {id: user._id, username, role, accessToken, refreshToken}});
-        //         } else {
-        //             const role= {menus: []}
-        //             // 返回登陆成功信息(包含user)
-        //             logger.info(`login successful, ${user.username}`);
-        //             return res.status(200).json({success: true, data: {id: user._id, username, role, accessToken, refreshToken}});
-        //         }
-        //     } else {
-        //         logger.warn('login failed, clientId is wrong。');
-        //         return res.status(400).json({success: false, errors: ['clientId不正确!']});
-        //     }
-        // } else {
-        //     // 登陆失败
-        //     logger.warn('login failed, username or password is wrong。');
-        //     return res.status(400).json({success: false, errors: ['用户名或密码不正确!']});
-        // }
-        return res.status(200).json({success: true, data: {id: "test"}});
+        if(user) {
+            logger.info("user exists")
+            //commenting client because there is no client.route and client.validation
+            //const client = await ClientModel.findOne({clientId: user.clientId});
+            //if case should contain if(client),but since client related data is not present, for now we are assuming that we have a client
+            // with the same clientID as the user, hence if(1)
+            if(1) {
+                const accessToken = generateToken({userId: user._id, createTime: new Date()}, 'AccessToken');
+                const refreshToken = generateToken({userId: user._id, createTime: new Date()}, 'RefreshToken');
+                await RefreshTokenModel.create({refreshToken, clientId: user.clientId, userId: user._id});
+                if (user.role_id) {
+                    const role = await RoleModel.findOne({_id: user.role_id});
+                    logger.info(`login success, ${user.username}`);
+                    return res.status(200).json({success: true, data: {username,accessToken, refreshToken}});
+                } else {
+                    const role= {menus: []}
+                    //returns login success information with the username
+                    logger.info(`login successful, ${user.username}`);
+                    return res.status(200).json({success: true, data: {username, accessToken, refreshToken}});
+                }
+            } else {
+                //when checking for the client, if the clientID is not same for the client and user then this else statement is true.
+                logger.warn('login failed, clientId is wrong。');
+                return res.status(400).json({success: false, errors: ['clientId is wrong!']});
+            }
+        } else {
+            // login failed
+            logger.warn('login failed, username or password is wrong。');
+            return res.status(400).json({success: false, errors: {errormessage:'login failed, username or password is wrong。',errorcode:'400'}});
+        }
+       
     } catch (err) {
         logger.error(`login failed, system error。${err}`);
-        return res.status(500).json({success: false, errors: ['登陆异常, 请重新尝试']});
+        return res.status(500).json({success: false, errors: {errormessage:'login failed, system error',errorcode:'500'}});
     }
 };
 
-// 添加用户
+//add a user
 const userAdd = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
     logger.info('The user add controller is started');
@@ -76,29 +82,29 @@ const userAdd = async (req, res, next) => {
             return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
         }
 
-        // 读取请求参数数据
+        // read req parameter data
         const {username, password} = req.body
-        // 处理: 判断用户是否已经存在, 如果存在, 返回提示错误的信息, 如果不存在, 保存
-        // 查询(根据username)
+        // if username already exists, return error or else create a new user
+        
         const user = await UserModel.findOne({username});
+        logger.info(user);
         if (user) {
-            // 返回提示错误的信息
             logger.warn(`the username already exists, ${user.username}`);
-            return res.status(400).json({success: false, errors: ['此用户已存在!']});
-        } else { // 没值(不存在)
-            // 保存
+            return res.status(400).json({success: false, errors: {errormessage:'user already exists',errorcode:'400'}});
+        } else { 
             await UserModel.create({...req.body, password: md5(password)});
-
+            const user = await UserModel.findOne({username});
+            logger.info(user)
             logger.info(`add user successful, ${user.username}`);
-            return res.status(200).json({success: true, data: user});
+            return res.status(200).json({success: true, data: {username}});
         }
     } catch (err) {
         logger.error(`add user failed, system error。${err}`);
-        return res.status(500).json({success: false, errors: ['添加用户异常, 请重新尝试!']});
+        return res.status(500).json({success: false, errors: {errormessage:'error, system error!',errorcode:'500'}});
     }
 };
 
-// 更新用户
+//user update
 const userUpdate = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
     logger.info('The user update controller is started');
@@ -108,16 +114,27 @@ const userUpdate = async (req, res, next) => {
         if (!validateResult.success) {
             return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
         }
-
+        
         const user = req.body;
+        
         const oldUser = await UserModel.findOneAndUpdate({_id: user._id}, user);
+        // after updating the user, we need to get the user object data without the password in it.
+        var dict={};
+        for(const i in Object.keys(user)){
+            var datum=Object.keys(user)[i];
+            if (datum!='password'){
+                dict[datum]=user[datum];}
+            
+        }
+       //dict returns the user information without the password in it.
         const data = Object.assign(oldUser, user);
-        // 返回
+       
+        
         logger.info(`update user successful, ${user.username}`);
-        return res.status(200).json({success: true, data: data});
+        return res.status(200).json({success: true, data:dict});
     } catch (err) {
         logger.error(`update user failed, system error。${err}`);
-        return res.status(500).json({success: false, errors: ['修改用户异常, 请重新尝试!']});
+        return res.status(500).json({success: false, errors: {errormessage:'update user failed,system error',errorcode:'500'}});
     }
 };
 
@@ -143,7 +160,7 @@ const userDelete = async (req, res, next) => {
     }
 };
 
-// 获取所有用户列表
+//list all the users that are present
 const userList = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
     logger.info('The user list controller is started');
@@ -155,13 +172,36 @@ const userList = async (req, res, next) => {
         }
 
         const users = await UserModel.find({username: {'$ne': 'admin'}});
-        const roles = await RoleModel.find();
-
+        //the following line can be uncommented if the users have roles defined
+        //const roles = await RoleModel.find();
+ 
+        //dict consists the infomration of all the users without the password included for returning as output
+        var dict={};
+        for(const i in Object.keys(users)){
+            var datum=users[i];
+            var dic={};
+            for(const j in Object.keys(datum)){
+            var dat=Object.keys(datum)[j];
+            
+            if(dat=='_doc'){
+                for(const k in Object.keys(datum[dat])){
+                var dat1= Object.keys(datum[dat])[k];
+                if (dat1!='password' && dat1!="__v"){
+                    dic[dat1]=datum[dat1];}
+                }
+                
+            }
+            }
+            
+        dict[i]=dic;  
+            
+        }
+    
         logger.info(`get user list successful.`);
-        return res.status(200).json({success: true, data: {users, roles}});
+        return res.status(200).json({success: true, data: Object.values(dict)});
     } catch (err) {
         logger.error(`get user list failed, system error。${err}`);
-        return res.status(500).json({success: false, errors: ['获取用户列表异常, 请重新尝试!']});
+        return res.status(500).json({success: false, errors: ['Get user list exception, please try again!']});
     }
 };
 
