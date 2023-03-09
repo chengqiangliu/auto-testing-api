4f9c9b2d36260ada1c8f90f29d79422bf6c5f761
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-03-09 15:45:54 +0900

    Add files via upload

---
+++ validations/tags.validation.js
@@ -0,0 +1,100 @@
+//removed body from the source code and added check and validation instead
+const { check, validationResult } = require("express-validator");
+
+exports.tagsAddValidator = 
+    [
+        check("tagsname")
+            .exists({checkFalsy: true})
+            .withMessage(" Tagname is required")
+            .isString()
+            .withMessage("Tagname should be string"),
+            (req, res, next) => {
+                const error = validationResult(req).formatWith(({ msg }) => ({msg,errorcode:'401'}));
+
+                const hasError = !error.isEmpty();
+
+                if (hasError) {
+                res.status(422).json({ errors: error.array() });
+                } else {
+                next();
+                }
+            }    
+    ];
+
+exports.tagsUpdateValidator = 
+     [
+        check("_id")
+            .exists({checkFalsy: true})
+            .withMessage("Application ID is required")
+            .isString()
+            .withMessage("Application ID should be string"),
+            (req, res, next) => {
+                const error = validationResult(req).formatWith(({ msg }) => ({msg,errorcode:'401'}));
+
+                const hasError = !error.isEmpty();
+
+                if (hasError) {
+                res.status(422).json({ errors: error.array() });
+                } else {
+                next();
+                }
+            }    
+            
+    ];
+
+
+exports.tagsDeleteValidator = 
+     [
+       check("tagsname")
+           .exists({checkFalsy: true})
+           .withMessage("Application name is required"),
+       (req, res, next) => {
+           const error = validationResult(req).formatWith(( {msg} ) => ({msg, errorcode:'400'}));
+           const hasError = !error.isEmpty();
+
+            if (hasError) {
+            res.status(400).json({ errors: error.array() });
+            } else {
+            next();
+            }
+        }
+    ];
+
+    exports.tagsDeleteByIdValidator = 
+    [
+       check("_id")
+           .exists({checkFalsy: true})
+           .withMessage("Application id is required"),
+       (req, res, next) => {
+           const error = validationResult(req).formatWith(( {msg} ) => ({msg, errorcode:'400'}));
+           const hasError = !error.isEmpty();
+ 
+           if (hasError) {
+           res.status(400).json({ error: error.array() });
+           } else {
+           next();
+           }
+       }
+   ]; 
+   
+   
+//    exports.appsInfoValidator = 
+//    [
+//        check("_id")
+//            .exists({checkFalsy: true})
+//            .withMessage("Application id is required")
+//            .isString()
+//            .withMessage("Application id should be string"),
+//            (req, res, next) => {
+//                const error = validationResult(req).formatWith(({ msg }) => ({msg,errorcode:'401'}));
+
+//                const hasError = !error.isEmpty();
+
+//                if (hasError) {
+//                res.status(422).json({ errors: error.array() });
+//                } else {
+//                next();
+//                }
+//            }    
+//    ];
+
