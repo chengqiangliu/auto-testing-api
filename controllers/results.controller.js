1e05fb01738cc63ba28561f92cd141386dc70861
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-03-10 12:36:01 +0900

    Add files via upload

---
+++ controllers/results.controller.js
@@ -0,0 +1,166 @@
+const path = require('path');
+
+const ResultsModel = require("../models/ResultsModel");
+const ErrorsModel = require("../models/ErrorsModel");
+const RunsModel = require("../models/RunsModel");
+const CasesModel = require("../models/CasesModel");
+const { validate } = require('./common.controller');
+
+const Constants = require('../lib/constants');
+const logger = require('../lib/logger').API;
+
+// adding a case tag
+const resultsAdd = async (req, res, next) => {
+    logger.addContext(Constants.FILE_NAME, path.basename(__filename));
+    logger.info('The case tags add controller is started');
+
+    try {
+        // validation
+        const validateResult = validate(req);
+        if (!validateResult.success) {
+            return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
+        }
+
+        // read request parameter data
+        const results = req.body
+        const Rerror = await ErrorsModel.findOne({_id:results.error});
+        const Rruns = await RunsModel.findOne({_id:results.run})
+        const Rcases= await CasesModel.findOne({_id:results.case})
+
+        if(Rerror && Rcases && Rruns){
+            // if results already exists, return error or else create a new result
+            const result = await ResultsModel.findOne({error:results.error,case:results.case,run:results.run});
+            if (result) {
+                logger.warn(`the result already exists`);
+                return res.status(400).json({success: false, error: {message:'result already exists',code:'400'}});
+            } else { 
+                await ResultsModel.create({...req.body});
+                const result = await ResultsModel.findOne({error:results.error,case:results.case,run:results.run});
+                logger.info(`add results successful`);
+                return res.status(200).json({success: true, data: result});
+            }
+        } else {
+            
+            logger.info(`add results failed, all required input data doesn't exist`);
+            return res.status(500).json({success: false, error: {message: 'add results failed, all required input data doesnt exist',code:'500'}});
+           
+        }
+        
+    } catch (err) {
+        logger.error(`add results failed, system error。${err}`);
+        return res.status(500).json({success: false, error: {message: 'add case tags failed, system error!',code:'500'}});
+    }
+};
+
+// results update
+const resultsUpdate = async (req, res, next) => {
+    logger.addContext(Constants.FILE_NAME, path.basename(__filename));
+    logger.info('The results update controller is started');
+    try {
+        // validation
+        const validateResult = validate(req);
+        if (!validateResult.success) {
+            return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
+        }
+
+        const results = req.body;
+
+        let Rerror = 1;
+        if ('error' in results){
+            logger.info('error attribute is present')
+            Rerror = await ErrorsModel.findOne({_id:results.error});
+            
+        }
+        let Rruns = 1;
+        if('run' in results){
+            logger.info('runs attribute is present')
+            Rruns = await RunsModel.findOne({_id:results.run})
+        } 
+        let Rcases=1;
+        if('case' in results){
+            logger.info('cases attribute is present')
+            Rcases= await CasesModel.findOne({_id:results.case})
+        }
+        
+        if(Rerror && Rcases && Rruns){
+        const oldresults = await ResultsModel.findOneAndUpdate({_id: results._id}, results);
+        // after updating the results, we need to get the results object data.
+        var dict={};
+        for(const i in Object.keys(results)){
+            var datum=Object.keys(results)[i];
+            if (datum!='__v'){
+                dict[datum]=results[datum];}
+            
+        }
+
+        //dict returns the apps information
+        const data = Object.assign(oldresults, results);
+        logger.info(`update results successful`);
+        return res.status(200).json({success: true, data: data});
+    } else {
+        logger.info('one of required attributes doesnt exist in the parent table');
+    }
+    } catch (err) {
+        logger.error(`update results failed, system error。${err}`);
+        return res.status(500).json({success: false, error: {message: 'update results failed, system error!',code:'500'}});
+    }
+};
+
+// results delete by ID
+const resultsDeleteById = async (req, res, next) => {
+    logger.addContext(Constants.FILE_NAME, path.basename(__filename));
+    logger.info('The results delete by _id controller is started');
+    try {
+        // validation
+        const validateResult = validate(req);
+        if (!validateResult.success) {
+            return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
+        }
+        const {_id} = req.body;
+        const results = await ResultsModel.findOne({_id});
+        if(results){
+            await ResultsModel.deleteOne({_id: _id});
+            logger.info(`delete results successful, ${_id}`);
+            return res.status(200).json({success: true, message: results._id + ' successfully deleted'});
+        }
+        else{
+            return res.status(404).json({success: false, error:[ {msg: results._id + ' does not exist', code: "404"}] });
+        }
+    } catch (err) {
+        logger.error(`delete results failed, system error。${err}`);
+        return res.status(500).json({success: false, error:[{ msg: 'delete results failed, system error!', code: '500'}]});
+    }
+};
+
+// fetch information of the results
+const resultsGetInfo = async (req, res, next) => {
+    logger.addContext(Constants.FILE_NAME, path.basename(__filename));
+    logger.info('The results info controller is started');
+    try {
+        // validation
+        const validateResult = validate(req);
+        if (!validateResult.success) {
+            return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
+        }
+
+        const {_id} = req.body;
+        const results = await ResultsModel.findOne({_id});
+        if(results){
+            return res.status(200).json({success:true, data: results});
+        }
+        else{
+            return res.status(404).json({success:false,error:[{msg:_id+'does not exist',code:"404"}]});
+        }
+    } catch (err) {
+        logger.error(`get case tags info failed, system error。${err}`);
+        return res.status(500).json({success: false, error: ['get results info failed, system error!']});
+    }
+};
+
+
+module.exports = {
+    resultsAdd,
+    resultsUpdate,
+    resultsDeleteById,
+    resultsGetInfo
+};
