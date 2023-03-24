b098dfbfa23e6b0609d3ada3c454a39903761fae
rashikarathi <123048487+rashikarathi@users.noreply.github.com>
2023-03-24 16:44:02 +0900

    Update ConfigurationModel.js

--- models/ConfigurationModel.js
+++ models/ConfigurationModel.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose')
 const moment = require("moment");
 //date formating to display it with the time as well
 const date = new Date();
-const formattedDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
-
+const Constants = require('../lib/constants');
+const formattedDate = moment(date).format(Constants.DATE_TIME_FORMAT);
 // 2.Defining Schema
 const configurationSchema = new mongoose.Schema({
   key: {type: String, required: true},
   value: {type: String, required: true},
   create_time: {type: String, default: formattedDate},
 },{versionKey: false})
+
+configurationSchema.virtual('id').get(function(){
+  return this._id.toHexString();
+});
+
+configurationSchema.set('toJSON', {
+  virtuals: true,
+  transform: function (doc, ret) {   delete ret._id  }
+});
 
 // 3. Creating configuration Model
 const ConfigurationModel = mongoose.model('configuration', configurationSchema)
