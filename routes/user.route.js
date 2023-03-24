b4dcfdbd0e9c514ab54cea126ae1bbd007c3afa1
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-03-24 17:12:11 +0900

    Add files via upload

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+
 const {
     userLoginValidator,
     userAddValidator,
@@ -18,24 +19,274 @@ const {
 } = require("../controllers/user.controller");
 const logger = require('../lib/logger').API;
 
-// login
-router.post('/login', function(req, res, next) {
-    let {username, password} = req.body;
-    logger.info(`username: ${username}, password: ${password}`)
-    return res.status(200).json({success: true, data: {id: "test"}});
-});
+const { expressjwt } = require('express-jwt');
+const { secret } = require('../config');
+const jwt = require('jsonwebtoken');
+//const logger = require('../lib/logger').API;
 
-// 添加用户
+const generateToken = (payload, type) => {
+    logger.info('The generateToken started');
+    if (type === 'AccessToken') {
+        return jwt.sign(payload, secret, {expiresIn: "600s"});
+    } else {
+        return jwt.sign(payload, secret, {expiresIn: "24h"});
+    }
+}
+// login
+// router.post('/login',  function(req, res, next) {
+//     let {username, password} = req.body;
+//     logger.info(`username: ${username}, password: ${password}`)
+//     //return res.status(200).json({success: true, data: {id: "test"}});
+//     const accessToken = generateToken({userId: 1, createTime: new Date()}, 'AccessToken');
+//     return res.status(200).json({success: true, data: {id: 1, username, accessToken}});
+// });
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *          type: Object
+ *          required:
+ *              -username
+ *              -password
+ *              -clientID
+ *          properties:
+ *              id:
+ *                 type: string
+ *                 description: The auto-generated id of the book
+ *              username:
+ *                 type: string
+ *                 description: the username of the user
+ *              password:
+ *                 type: string
+ *                 description: the password of the user
+ *              phone:
+ *                 type: string
+ *                 description: the phone details of the user
+ *              email:
+ *                 type: string
+ *                 description: email address of the user
+ *              clientID:
+ *                 type: string
+ *                 description: clientID related to the user
+ *
+ */
+/**
+  * @swagger
+  * tags:
+  *   name: User
+  *   description: Users managing API
+  */
+
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: Login the present user 
+ *     tags: [User]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     parameters:
+ *       - in: formData
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The username of the user
+ *       - in: formData
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The password of the user
+ *       - in: formData
+ *         name: clientID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The clientID associated with the user
+ *     responses:
+ *       200:
+ *         description: The user is logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
+router.post('/login',userLoginValidator,userLogin);
+
+/**
+ * @swagger
+ * /api/users/add:
+ *   post:
+ *     summary: add a new user 
+ *     tags: [User]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     parameters:
+ *       - in: formData
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The username of the user
+ *       - in: formData
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The password of the user
+ *       - in: formData
+ *         name: clientID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The clientID associated with the user
+ *       - in: formData
+ *         name: phone
+ *         schema:
+ *           type: string
+ *         description: The phone details associated with the user
+ *       - in: formData
+ *         name: email
+ *         schema:
+ *           type: string
+ *         description: The email address associated with the user
+ *     responses:
+ *       200:
+ *         description: The user is added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 router.post('/add', userAddValidator, userAdd);
 
-// 更新用户
+/**
+ * @swagger
+ * /api/users/update:
+ *   post:
+ *     summary: update a user 
+ *     tags: [User]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     parameters:
+ *       - in: formData
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The username of the user
+ *       - in: formData
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The password of the user
+ *       - in: formData
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The id associated with the user
+ *       - in: formData
+ *         name: phone
+ *         schema:
+ *           type: string
+ *         description: The phone details associated with the user
+ *       - in: formData
+ *         name: email
+ *         schema:
+ *           type: string
+ *         description: The email address associated with the user
+ *     responses:
+ *       200:
+ *         description: The user is added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 router.post('/update', userUpdateValidator, userUpdate);
 
-// 删除用户
-router.post('/deleteByUser', userDeleteValidator, userDelete);
+/**
+ * @swagger
+ * /api/users/delete:
+ *   post:
+ *     summary: delete a user
+ *     tags: [User]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     parameters:
+ *       - in: formData
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user id of the user
+ *     responses:
+ *       200:
+ *         description: The user is added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+router.post('/delete', userDeleteValidator, userDelete);
+
+/**
+ * @swagger
+ * /api/users/deleteById:
+ *   post:
+ *     summary: delete a user
+ *     tags: [User]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     parameters:
+ *       - in: formData
+ *         name: client Id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The client id of the user
+ *     responses:
+ *       200:
+ *         description: The user is added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 router.post('/deleteById', userDeleteByIdValidator, userDeleteById);
 
-// 获取所有用户列表
-router.post('/list', userList);
+/**
+ * @swagger
+ * /api/users/list:
+ *   get:
+ *     summary: get the list of all users in the database
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: The user is added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+router.get('/list', userList);
 
 module.exports = router;
