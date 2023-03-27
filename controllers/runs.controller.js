d0894588cbddb7adceac613bda681d24eb7dbd91
rashikarathi <123048487+rashikarathi@users.noreply.github.com>
2023-03-27 19:21:24 +0900

    Update runs.controller.js

--- controllers/runs.controller.js
+++ controllers/runs.controller.js
@@ -3,10 +3,12 @@ const path = require('path');
 const RunsModel = require("../models/RunsModel");
 const JobsModel = require("../models/JobsModel");
 const { validate } = require('./common.controller');
+const UserModel = require('../models/UserModel');
 
 const Constants = require('../lib/constants');
 const logger = require('../lib/logger').API;
 const errorStatements = require('../lib/errorStatements');
+const {autho} = require('../auth/index')
 
 // adding 
 const runsAdd = async (req, res, next) => {
@@ -22,6 +24,11 @@ const runsAdd = async (req, res, next) => {
 
         // read request parameter data
         const {job_id} = req.body
+        //accesstoken checking
+        const username = autho(req)
+        let user = await UserModel.findOne({username:username});
+        let userid = user._id
+
         const jobs = await JobsModel.findOne({_id:job_id});
         if(jobs){
             // if job_id already exists, return error or else create a new runs
@@ -30,7 +37,7 @@ const runsAdd = async (req, res, next) => {
                 logger.warn(`the runs already exists`);
                 return res.status(400).json({success: false, error: {message:'runs already exists',code:'400'}});
             } else { 
-                await RunsModel.create({...req.body});
+                await RunsModel.create({create_user:userid,update_user:userid,...req.body});
                 const runs = await RunsModel.findOne({job_id:job_id});
                 logger.info(`add runs successful`);
                 return res.status(200).json({success: true, data: {id: runs._id, jobs, created_at: runs.created_at, finished_at: runs.finished_at, total: runs.total, status: runs.status}});
@@ -45,7 +52,7 @@ const runsAdd = async (req, res, next) => {
         
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
-         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.message), code: 500}]});
+         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.split("|")[1]), code: 500}]});
     }
 };
 
@@ -53,7 +60,7 @@ const runsAdd = async (req, res, next) => {
 // runs delete by ID
 const runsDeleteById = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
-    logger.info('The runs delete by _id controller is started');
+    logger.info('The runs delete by id controller is started');
     try {
         // validation
         const validateResult = validate(req);
@@ -72,7 +79,7 @@ const runsDeleteById = async (req, res, next) => {
         }
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
-         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.message), code: 500}]});
+         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.split("|")[1]), code: 500}]});
     }
 };
 
@@ -99,7 +106,7 @@ const runsGet = async (req, res, next) => {
         }
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
-         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.message), code: 500}]});
+         return res.status(500).json({success: false, error: [{message : (errorStatements.CatchBlockErr.split("|")[1]), code: 500}]});
     }
 };
 
