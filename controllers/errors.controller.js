60d43bc7e93af2b62a00a6c2207db8bd17add12a
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-03-28 15:03:51 +0900

    Update errors.controller.js

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -4,6 +4,7 @@ const ErrorsModel = require("../models/ErrorsModel");
 const UserModel = require('../models/UserModel');
 const { validate } = require('./common.controller');
 const {autho} = require('../auth/index')
+const errorStatements = require('../lib/errorStatements');
 
 const Constants = require('../lib/constants');
 const logger = require('../lib/logger').API;
