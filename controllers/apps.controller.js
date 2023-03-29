82a5b79778aa08033e881fc645feeeab57eeb5ff
rashikarathi <123048487+rashikarathi@users.noreply.github.com>
2023-03-29 20:36:05 +0900

    Update apps.controller.js

--- controllers/apps.controller.js
+++ controllers/apps.controller.js
@@ -22,23 +22,22 @@ const appsAdd = async (req, res, next) => {
         }
 
         // read request parameter data
-        const {appsname} = req.body
+        const {apps_name} = req.body
         //accesstoken checking
         const username = autho(req)
         let user = await UserModel.findOne({username:username});
         let userid = user._id
 
-        // if appsname already exists, return error or else create a new apps
-        const apps = await AppsModel.findOne({appsname});
-        logger.info(apps);
+        // if apps_name already exists, return error or else create a new apps
+        const apps = await AppsModel.findOne({apps_name});
         if (apps) {
-            logger.warn(`the Application name already exists, ${apps.appsname}`);
+            logger.warn(`the Application name already exists, ${apps.apps_name}`);
             return res.status(400).json({success: false, errors: {errormessage:'Application already exists',errorcode:'400'}});
         } else { 
             await AppsModel.create({create_user:userid,update_user:userid,...req.body});
-            const apps = await AppsModel.findOne({appsname});
+            const apps = await AppsModel.findOne({apps_name});
             logger.info(apps)
-            logger.info(`add apps successful, ${apps.appsname}`);
+            logger.info(`add apps successful, ${apps.apps_name}`);
             return res.status(200).json({success: true, data: apps});
         }
     } catch (err) {
@@ -63,8 +62,7 @@ const appsUpdate = async (req, res, next) => {
         const username = autho(req)
         let user = await UserModel.findOne({username:username});
         let userid = user._id
-        tags['update_user']=userid;
-
+        apps['update_user']=userid;
         const oldApps = await AppsModel.findOneAndUpdate({id: apps.id}, apps);
         // after updating the apps, we need to get the apps object data.
         var dict={};
@@ -77,7 +75,7 @@ const appsUpdate = async (req, res, next) => {
 
         //dict returns the apps information
         const data = Object.assign(oldApps, apps);
-        logger.info(`update apps successful, appsName: ${apps.appsname}, appsId: ${apps.id}`);
+        logger.info(`update apps successful, apps_name: ${apps.apps_name}, appsId: ${apps.id}`);
         return res.status(200).json({success: true, data: data});
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
@@ -88,22 +86,22 @@ const appsUpdate = async (req, res, next) => {
 // apps delete by name
 const appsDelete = async (req, res, next) => {
     logger.addContext(Constants.FILE_NAME, path.basename(__filename));
-    logger.info('The apps delete by Appsname controller is started');
+    logger.info('The apps delete by apps_name controller is started');
     try {
         // validation
         const validateResult = validate(req);
         if (!validateResult.success) {
             return res.status(validateResult.status).json({success: false, errors: validateResult.errors});
         }
-        const {appsname} = req.body;
-        const apps = await AppsModel.findOne({appsname});
+        const {apps_name} = req.body;
+        const apps = await AppsModel.findOne({apps_name});
         if(apps){
-            await AppsModel.deleteOne({appsname: appsname});
-            logger.info(`delete apps successful, ${appsname}`);
-            return res.status(200).json({success: true, message: appsname + ' successfully deleted'});
+            await AppsModel.deleteOne({apps_name: apps_name});
+            logger.info(`delete apps successful, ${apps_name}`);
+            return res.status(200).json({success: true, message: apps_name + ' successfully deleted'});
         }
         else{
-            return res.status(404).json({success: false, error:[ {msg: appsname + ' does not exist', errorcode: "404"}] });
+            return res.status(404).json({success: false, error:[ {msg: apps_name + ' does not exist', errorcode: "404"}] });
         }
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
@@ -126,10 +124,10 @@ const appsDeleteById = async (req, res, next) => {
         if(apps){
             await AppsModel.deleteOne({id: id});
             logger.info(`delete apps successful, ${id}`);
-            return res.status(200).json({success: true, message: apps.appsname + ' successfully deleted'});
+            return res.status(200).json({success: true, message: apps.apps_name + ' successfully deleted'});
         }
         else{
-            return res.status(404).json({success: false, error:[ {msg: apps.appsname + ' does not exist', errorcode: "404"}] });
+            return res.status(404).json({success: false, error:[ {msg: apps.apps_name + ' does not exist', errorcode: "404"}] });
         }
     } catch (err) {
         logger.error(JSON.stringify(errorStatements.CatchBlockErr)+`${err}`);
