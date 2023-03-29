7509fd81c5b7f9f6803e8e973e73ee68b0cf84ec
VenkataSChirumarri <120075511+VenkataSChirumarri@users.noreply.github.com>
2023-03-29 17:46:57 +0900

    Update CasesModel.js

--- models/CasesModel.js
+++ models/CasesModel.js
@@ -15,7 +15,7 @@ const casesSchema = new mongoose.Schema({
   location:{type:String},
   reference:{type:String},
   create_time: {type: String, default: formattedDate},
-  deleteFlag:{type:Number ,default:0,required:true},
+  delete_flag:{type:Number ,default:0,required:true},
   create_user:{type:String},
   update_user:{type:String}
 },{versionKey:false},{
